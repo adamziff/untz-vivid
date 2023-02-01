ae1b4a9f427b8e1a54db8721447a54deb037b125
Adam Ziff <adamhziff@gmail.com>
2023-02-01 15:40:59 -0500

    align button center

--- pages/host/new-untz.tsx
+++ pages/host/new-untz.tsx
@@ -1,10 +1,31 @@
 import Link from "next/link"
+import styles from '../../styles/Home.module.css'
+import Head from "next/head"
 
 export default function NewUntz() {
     return (
-        <div>
-            <h1>New Untz</h1>
-            <Link href='/'>Home</Link>
+        <div className={styles.container}>
+            <Head>
+            <title>üntz</title>
+            <meta name="description" content="playlists for every party" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className="flex flex-col">
+            <Link href="/">Home</Link>
+            <h1 className="basis-full text-7xl font-bold text-center text-white">
+              new üntz
+            </h1>
+
+            <p className="basis-full text-emerald-300 p-10 text-center">
+                your next party starts here
+            </p>
+            <div className="flex flex-col justify-center items-center">
+              <button className="bg-emerald-300 text-black rounded-md px-10 py-1 font-bold">
+                <Link href="/host/new-untz">share üntz</Link>
+              </button>
+            </div>
+        </main>
         </div>
     )
 }
