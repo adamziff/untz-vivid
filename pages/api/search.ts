e3236c9924c210774f202d82a23857e61ff29edb
Adam Ziff <adamhziff@gmail.com>
2023-02-13 11:38:36 -0500

    fix 504 attempt 2

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,5 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -11,54 +10,99 @@ const clientSecret = process.env.CLIENT_SECRET;
 // Create the api object with the credentials
 var spotifyApi = new SpotifyWebApi({
   clientId: clientId,
-  clientSecret: clientSecret
+  clientSecret: clientSecret,
 });
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-// CORS middleware
-const corsMiddleware = (req: NextApiRequest, res: NextApiResponse, next: Function) => {
-  const allowedOrigins = ['www.untz.studio', 'untz-vivid.vercel.app', 'untz-vivid-adamziff.vercel.app'];
-  const origin = req.headers.origin ? req.headers.origin : '';
-  if (allowedOrigins.includes(origin)) {
-  res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-  };
+const allowedOrigins = [
+  'www.untz.studio',
+  'untz-vivid.vercel.app',
+  'untz-vivid-adamziff.vercel.app',
+];
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-
-  // Use the CORS middleware
-  corsMiddleware(req, res, () => {});
+  // Set up CORS
+  const origin = req.headers.origin ? req.headers.origin : '';
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
 
   // Retrieve an access token.
   spotifyApi.clientCredentialsGrant().then(
-    function(data: any) {
-      // console.log('The access token expires in ' + data.body['expires_in']);
-      // console.log('The access token is ' + data.body['access_token']);
-
+    (data: any) => {
       // Save the access token so that it's used in future calls
       spotifyApi.setAccessToken(data.body['access_token']);
+
+      spotifyApi
+        .searchTracks(req.query.q, { limit: 5 })
+        .then(
+          (data: any) => {
+            console.log('Search', data.body);
+            res.status(200).json(data.body);
+          },
+          (err: any) => {
+            console.error(err);
+          }
+        );
     },
-    function(err: any) {
+    (err: any) => {
       console.log('Something went wrong when retrieving an access token', err);
     }
   );
-  // console.log('req.query.q');
-  // console.log(req.query.q);
-  spotifyApi.searchTracks(req.query.q, {limit: 5})
-  .then(function(data: any) {
-    console.log('Search', data.body);
-    res.status(200).json(data.body)
-  }, function(err: any) {
-    console.error(err);
-  });
 }
+
+
+
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import * as dotenv from 'dotenv';
+// dotenv.config();
+
+// var SpotifyWebApi = require('spotify-web-api-node');
+
+// const clientId = 'f5e1c0e33ba2435ca10ef8beb593984d';
+// const clientSecret = process.env.CLIENT_SECRET;
+
+// // Create the api object with the credentials
+// var spotifyApi = new SpotifyWebApi({
+//   clientId: clientId,
+//   clientSecret: clientSecret
+// });
+
+// type Data = {
+//   name: string
+// }
+
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+
+//   // Retrieve an access token.
+//   spotifyApi.clientCredentialsGrant().then(
+//     function(data: any) {
+//       // console.log('The access token expires in ' + data.body['expires_in']);
+//       // console.log('The access token is ' + data.body['access_token']);
+
+//       // Save the access token so that it's used in future calls
+//       spotifyApi.setAccessToken(data.body['access_token']);
+//     },
+//     function(err: any) {
+//       console.log('Something went wrong when retrieving an access token', err);
+//     }
+//   );
+  
+//   spotifyApi.searchTracks(req.query.q, {limit: 5})
+//   .then(function(data: any) {
+//     console.log('Search', data.body);
+//     res.status(200).json(data.body)
+//   }, function(err: any) {
+//     console.error(err);
+//   });
+// }
