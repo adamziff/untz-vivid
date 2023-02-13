31894245d8ab19640296e20669559703ff5769c7
Adam Ziff <adamhziff@gmail.com>
2023-02-13 11:30:16 -0500

    fix 504 attempt 1

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -18,10 +18,26 @@ type Data = {
   name: string
 }
 
+// CORS middleware
+const corsMiddleware = (req: NextApiRequest, res: NextApiResponse, next: Function) => {
+  const allowedOrigins = ['www.untz.studio', 'untz-vivid.vercel.app', 'untz-vivid-adamziff.vercel.app'];
+  const origin = req.headers.origin ? req.headers.origin : '';
+  if (allowedOrigins.includes(origin)) {
+  res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+  };
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  // Use the CORS middleware
+  corsMiddleware(req, res, () => {});
 
   // Retrieve an access token.
   spotifyApi.clientCredentialsGrant().then(
