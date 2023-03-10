053028fa0a4f5b65808c407adb6a5bbb5d3c3abd
Adam Ziff <adamhziff@gmail.com>
2023-03-10 13:00:23 -0500

    dont print tokens

--- pages/api/create-playlist.ts
+++ pages/api/create-playlist.ts
@@ -58,10 +58,10 @@ export default async function handler(
         console.log('create-playlist.ts: starting authcodegrant')
 
         // const { body: { access_token, refresh_token } } = await spotifyApi.authorizationCodeGrant(authCode);
-        console.log('access_token')
-        console.log(access_token)
-        console.log('refresh_token')
-        console.log(refresh_token)
+        // console.log('access_token')
+        // console.log(access_token)
+        // console.log('refresh_token')
+        // console.log(refresh_token)
         spotifyApi.setAccessToken(access_token);
         spotifyApi.setRefreshToken(refresh_token);
 
